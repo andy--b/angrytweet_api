51811a744c8e0911bde4775ad905898bfc2f5043
Andy <andrew.r.bowling@gmail.com>
2015-10-29 19:03:22 -0400

    Finish FTs, all pass. Ready for deploy

--- functional_tests.py
+++ functional_tests.py
@@ -52,6 +52,11 @@ class NavigationTest(unittest.TestCase):
 		inputbox.send_keys('Bacon')
 		inputbox.send_keys(Keys.ENTER)
 		
+		# He notices the page title and header mention mean search results
+		self.assertIn('Search Results', self.browser.title)
+		header_text = self.browser.find_element_by_id('header-text').text
+		self.assertIn('SEARCH RESULTS', header_text)
+		
 		# Navbar is still present on this new page
 		testNavBar(self)
 		
@@ -59,7 +64,57 @@ class NavigationTest(unittest.TestCase):
 		search_term = self.browser.find_element_by_id('search-result-term').text
 		self.assertIn('BACON', search_term)
 		
-		self.fail('finish me!')
+		# He sees a tweet, refreshes the page, and sees the same tweet
+		tweet_text = self.browser.find_element_by_id('result-tweet-text').text
+		self.browser.refresh()
+		self.assertEqual(tweet_text, 
+						 self.browser.find_element_by_id('result-tweet-text').text)
+		
+		# Since this is the first of the results list, he sees no "previous" button
+		self.assertEqual(0, len(self.browser.find_elements_by_id('result-previous')))
+		
+		# He sees a next button, hits it, and sees a different tweet-text
+		next_button = self.browser.find_element_by_id('result-next')
+		next_button.click()
+		self.assertNotEqual(tweet_text, 
+						 self.browser.find_element_by_id('result-tweet-text').text)
+						 
+		# Satisfied with his awesome bacon search, Andy now goes to the top 10 page
+		navbar_top = self.browser.find_element_by_id('nav-top')
+		navbar_top.click()
+		
+		# He notices the page title and header mention mean search results
+		self.assertIn('Top Tweets of All Time', self.browser.title)
+		header_text = self.browser.find_element_by_id('header-text').text
+		self.assertIn('TOP TWEETS OF ALL TIME', header_text)
+		
+		# Navbar is still present on this new page
+		testNavBar(self)
+		
+		# He sees 10 top tweets, each inside a jumbotron
+		self.assertEqual(10,
+						 len(self.browser.find_elements_by_class_name('jumbotron')))
+		
+		# Finally, he navigates to the random tweet section of the website
+		navbar_random = self.browser.find_element_by_id('nav-random')
+		navbar_random.click()
+		
+		# Navbar is still present on this new page
+		testNavBar(self)
+		
+		# He sees a tweet
+		self.assertEqual(1,
+						 len(self.browser.find_elements_by_class_name('jumbotron')))
+						 
+		# He likes this tweet, but not enough to vote. He wants another
+		tweet_text = self.browser.find_element_by_id('random-tweet-text').text
+		another_button = self.browser.find_element_by_id('random-another-btn')
+		another_button.click()
+		
+		# He sees another tweet, still has a navbar
+		testNavBar(self)
+		self.assertNotEqual(tweet_text,
+							self.browser.find_element_by_id('random-tweet-text').text)
 		
 if __name__ == '__main__':
     unittest.main(warnings='ignore')
